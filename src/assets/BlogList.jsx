7c9be17c0ed73b111bf2d9a75ac79129e6bce4a4
Bhaskardwivedi <bhaskardwivedi544@gmail.com>
2025-08-06 00:23:49 +0530

    upating background images on all page

--- src/assets/BlogList.jsx
+++ src/assets/BlogList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import bgImage from '../assets/bg-blogs.jpg';
+import bgImage from '../assets/bg-blog.jpg';
 
 const BlogList = () => {
   const [blogs, setBlogs] = useState([]);
